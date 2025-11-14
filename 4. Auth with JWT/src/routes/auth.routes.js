d58231caee0dd589e34c542cbe62be09cb6f6450
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-14 18:38:02 +0530

    Added JWT token while login and logout api is also implemented

--- 4. Auth with JWT/src/routes/auth.routes.js
+++ 4. Auth with JWT/src/routes/auth.routes.js
@@ -8,6 +8,12 @@ const router = express.Router();
 router.post('/register', async (req, res) => {
     const { username, name, email, password } = req.body;
 
+    const usernameExists = await userModel.findOne({ username });
+
+    if (usernameExists) {
+        return res.status(409).json({ message: "Username already in use" });
+    }
+
     const user = await userModel.create({
         username,
         name,
@@ -18,6 +24,7 @@ router.post('/register', async (req, res) => {
     // Token creation
     // Note: Generate JWT SECRET using website -> 'https://jwtsecrets.com/'
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+
     res.cookie("token", token);
 
     res.status(201).json({ message: "User registered successfully!!", user, token });
@@ -41,7 +48,13 @@ router.post('/login', async (req, res) => {
         return res.status(401).json({ message: "Invalid Password" });
     }
 
-    res.status(200).json({ message: "User logged in successfully" });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+
+    res.cookie('token', token, {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 2), // 2 days
+    });
+
+    res.status(200).json({ message: "User logged in successfully", user });
 });
 
 router.get('/user', async (req, res) => {
@@ -49,11 +62,11 @@ router.get('/user', async (req, res) => {
     console.log(req.cookies);
     
     if (!token) {
-        return res.status(401).json({ message: "Unauthorised" });
+        return res.status(401).json({ message: "Unauthorized" });
     }
 
     try {
-        // jwt.verify() checks that our token is correct or not
+        // jwt.verify() checks that our token is correct or not and created by server only or someone else
         const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
         // We can use .select() to omit sending password and version at client side
@@ -69,6 +82,12 @@ router.get('/user', async (req, res) => {
             message: "Unauthorised - Invalid Token"
         })
     }
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('token');
+
+    res.status(200).json({ message: "User logged out successfully" });
 });
 
 module.exports = router;
