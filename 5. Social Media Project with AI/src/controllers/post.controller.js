88a228f924f3bb8f75535c0b4f39b9514ad48dca
Sandeep <sandeepwadhawan2001@gmail.com>
2025-11-18 22:22:00 +0530

    Generated image caption via gemini-ai in post controller

--- 5. Social Media Project with AI/src/controllers/post.controller.js
+++ 5. Social Media Project with AI/src/controllers/post.controller.js
@@ -1,5 +1,20 @@
-const createPost = async (req, res) => {
+const postModel = require("../models/post.model");
+const generateCaption = require("../service/ai.service");
+const uploadFile = require("../service/storage.service");
 
+const createPost = async (req, res) => {
+    const fileData = await uploadFile(req.file);
+
+    const base64Image = Buffer.from(req.file.buffer).toString('base64');
+
+    const caption = await generateCaption(base64Image);
+
+    // const post = await postModel.create({
+    //     postImg: fileData.url,
+    //     caption
+    // });
+    
+    res.json({ caption });
 };
 
 module.exports = { createPost };
